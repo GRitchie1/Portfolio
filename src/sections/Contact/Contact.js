e8e134c7dd5e382e03dff6a13f0284aa1adbd75e
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-09 14:04:24 +0100

    Fix Link styling

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
       <div id="contactme" className="Contact-Section">
         <header className="Contact-Header">
           <h1 >Contact Me</h1>
-          <a href="https://www.linkedin.com/in/gregorritchie/" >
+          <a className="Contact-Links" href="https://www.linkedin.com/in/gregorritchie/" >
             <p> Contact me on LinkedIn</p>
           </a>
         </header>
