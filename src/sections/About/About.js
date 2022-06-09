8bd8f15d07bb6f32edb495b1fd9bb6f998c8a837
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-09 10:26:54 +0100

    Fix alignment of text/image

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -7,6 +7,7 @@ function About() {
         <header className="About-Header">
           <h1>About Me</h1>
           <p> I am an aspiring software engineer with a passion for systems development and mechatronics.  I have been leading the development of mechatronics projects for the past 3 years.  I have built several personal projects outside of work which can be seen below.</p>
+          <p> If I'm not at my desk, you can probably find me fixing my classic VW Van or riding my motorbike.</p>
         </header>
         <img src={image} className="About-Image" alt="Gregor Ritchie" />
       </div>
