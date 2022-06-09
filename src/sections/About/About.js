0e19790182f5b89d57e8508a20cfdbd1616a16a0
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-09 10:20:31 +0100

    Add Image to About

--- src/sections/About/About.js
+++ src/sections/About/About.js
@@ -1,12 +1,14 @@
 import './About.css';
+import image from './image.jpg'
 
 function About() {
     return (
       <div className="About-Section">
         <header className="About-Header">
           <h1>About Me</h1>
-          <p> I am an aspiring software engineer with a passion for systems development and mechatronics.</p>
+          <p> I am an aspiring software engineer with a passion for systems development and mechatronics.  I have been leading the development of mechatronics projects for the past 3 years.  I have built several personal projects outside of work which can be seen below.</p>
         </header>
+        <img src={image} className="About-Image" alt="Gregor Ritchie" />
       </div>
     );
   }
--- src/sections/Main/Main.js
+++ src/sections/Main/Main.js
@@ -1,4 +1,4 @@
-import logo from '../../Spiral.png';
+//import logo from '../../Spiral.png';
 import './Main.css';
 
 function Main() {
@@ -7,7 +7,7 @@ function Main() {
       <header className="Main-header">
         <h1>I am Gregor Ritchie</h1>
         <p> I am an aspiring software engineer with a passion for systems development and mechatronics.</p>
-        
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
         <div className="Main-Button">
           &#8595;
         </div>
