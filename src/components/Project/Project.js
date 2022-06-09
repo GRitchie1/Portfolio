813a0d64ec956e789173c4dc648b07be6ea49ea8
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-09 12:23:11 +0100

    Add some projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -7,14 +7,18 @@ export class Project extends React.Component {
       this.title = props.title
       this.img = props.img
       this.content = props.content
+      this.readmore = props.readmore
     }
     
     render() {
       return (
         <div className={styles.ProjectContainer}>
-          <img src={this.img} alt={this.title}></img>
-          <h3>{this.title}</h3>
-          <p>{this.content}</p>
+          <img className={styles.ProjectImg} src={this.img} alt={this.title}></img>
+          <div className={styles.ProjectTextContainer}>
+            <h3 className={styles.ProjectText}>{this.title}</h3>
+            <p className={styles.ProjectText}>{this.content}</p>
+            <a className={styles.ProjectText} href={this.readmore}>Read More</a>
+          </div>
         </div>
       );
     }
--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Projects.css';
 import Project from '../../components/Project/Project';
+import img1 from './Images/1.jpg';
+import img2 from './Images/2.jpg';
 
 export class Projects extends React.Component {
   constructor(props){
     super(props);
     this.projectslist = [
-      {title:"This is the 1st project.", content:"This project was great!", img:"src" },
-      {title:"This is the 2nd project.", content:"This project awesome!", img:"src" }
+      {title:"RFID Robotic ARM", content:"As part of my HND Mechatronics course I built an RFID Robotic arm to pick up, identify, and sort items based on their RFID codes.", img:img1, readmore:"https://github.com/GRitchie1/RFIDRoboticArm-HNDGradedUnit" },
+      {title:"Automatic GreenHouse", content:"I am currently developing a modular automatic greenhouse that can water the plants and control the air temperature & humidity.", img:img2, readmore:"https://github.com/GRitchie1/AutomaticGreenhouse" },
     ]
   }
   render() {
@@ -15,8 +17,8 @@ export class Projects extends React.Component {
       <div id="projects" className="Projects-Section">
         <header className="Projects-Header">
           <h1 >Projects</h1>
-          <p> Here are some of my past projects.</p>
-          {this.projectslist.map((project)=>{return(<Project title={project.title} content={project.content} img={project.img}/>);})}
+          <p> Here are some of my current and past projects.</p>
+          {this.projectslist.map((project)=>{return(<Project title={project.title} content={project.content} img={project.img} readmore={project.readmore}/>);})}
         </header>
       </div>
     );
