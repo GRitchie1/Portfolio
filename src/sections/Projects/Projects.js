0945fe35592c60587a953702714c675b385ded79
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-10 22:08:46 +0100

    Add load more button to projects

--- src/sections/Projects/Projects.js
+++ src/sections/Projects/Projects.js
@@ -11,6 +11,22 @@ export class Projects extends React.Component {
       {title:"RFID Robotic Arm", content:"As part of my HND Mechatronics course I built an RFID Robotic arm to pick up, identify, and sort items based on their RFID codes.  This project was 3D printed on my Ender3V2.", img:img1, readmore:"https://github.com/GRitchie1/RFIDRoboticArm-HNDGradedUnit" },
       {title:"Automatic GreenHouse", content:"I am currently developing a modular automatic greenhouse that can water the plants and control the air temperature & humidity.", img:img2, readmore:"https://github.com/GRitchie1/AutomaticGreenhouse" },
     ]
+    this.projectsToShow=[this.projectslist[0]]
+    this.pageSize = 1               //Number of projects per page
+    this.state = { loadNumber: 1 }; //Default number of pages to show
+  }
+  handleClick = () => {
+    this.setState(({ loadNumber }) => ({
+      loadNumber: loadNumber + 1
+    }))
+
+    this.projectsToShow = this.projectslist.slice(0,((this.pageSize*this.state.loadNumber)+1))
+  }
+  loadMoreButton(){
+    if (this.state.loadNumber*this.pageSize < this.projectslist.length){
+      return(<button onClick={this.handleClick}>Load More</button>)
+    }
+
   }
   render() {
     return (
@@ -20,9 +36,10 @@ export class Projects extends React.Component {
           <p> Here are some of my current and past projects.</p>
         </header>
           <div className='Projects-Container'>
-            {this.projectslist.map((project)=>{return(<Project title={project.title} content={project.content} img={project.img} readmore={project.readmore}/>);})}
+            {this.projectsToShow.map((project)=>{return(<Project title={project.title} content={project.content} img={project.img} readmore={project.readmore}/>);})}
           </div>
-        
+        {this.loadMoreButton()}
+          
       </div>
     );
   }
