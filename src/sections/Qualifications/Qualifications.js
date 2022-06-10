2a0bd820cdcb9aacb37280d265867d206f94f5cc
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-10 12:22:08 +0100

    Add qualifications links

--- src/sections/Qualifications/Qualifications.js
+++ src/sections/Qualifications/Qualifications.js
@@ -1,4 +1,7 @@
 import './Qualifications.css';
+import codecademy from './Codecademy.png'
+import htb from './htb.jpg'
+import transcript from './GRitchie1 HTB Transcript.pdf'
 
 function Qualifications() {
     return (
@@ -6,8 +9,17 @@ function Qualifications() {
         <header className="Qualification-Header">
           <h1 >Qualifications</h1>
           <p> I am currently studying HND Mechatroncs at the City of Glasgow College, Scotland.</p>
-          <p> I have completed my HNC Mechatronics and recieved an A for my Graded Unit.  I have also undertaken many online courses on CodeCademy and Hack the Box Academy.</p>
+          <p> I have completed my HNC Mechatronics and recieved an A for my Graded Unit.  </p>
+          <p>I have also undertaken many online courses on CodeCademy and Hack the Box Academy.</p>
         </header>
+        <div className='Qualification-LinksContainer'>
+          <a className='Qualification-Links' href="https://www.codecademy.com/profiles/GRitchie1">
+            <img src={codecademy} alt="Codecademy"></img>
+          </a>
+          <a className='Qualification-Links' href={transcript}>
+            <img src={htb} alt="Hack the Box Academy"></img>
+          </a>
+        </div>
       </div>
     );
   }
