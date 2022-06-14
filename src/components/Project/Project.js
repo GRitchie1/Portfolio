81d0965440712816eb29aa32db3e7006f4a9d7f5
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-14 14:08:27 +0100

    Add open project which replaces list of projects

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -2,27 +2,51 @@ import React from 'react';
 import styles from './Project.module.css';
 import Button from '../Button/Button'
 
-export class Project extends React.Component {
+class Project extends React.Component {
     constructor(props){
       super(props);
       this.title = props.title
       this.img = props.img
       this.content = props.content
-      this.readmore = props.readmore
+      this.url = props.url
+      this.index = props.index
     }
     
     render() {
+      var index = this.index
       return (
         <div className={styles.ProjectContainer}>
           <img className={styles.ProjectImg} src={this.img} alt={this.title}></img>
           <div className={styles.ProjectTextContainer}>
             <h3 className={styles.ProjectText}>{this.title}</h3>
             <p className={styles.ProjectText}>{this.content}</p>
-            <Button link={this.readmore} text="Read More"/>
+            <Button link={this.url} text="Read More"/>
+            <button onClick={()=>this.props.handler(index)}>open</button>
           </div>
         </div>
       );
     }
   }
 
-  export default Project;
+
+  class OpenProject extends React.Component {
+    constructor(props){
+      super(props);
+      this.title = props.title
+      this.img = props.img
+      this.content = props.content
+      this.url = props.url
+    }
+    
+    render() {
+      return (
+        <div className={styles.OpenProjectContainer}>
+          This is an open project
+        </div>
+      );
+    }
+  }
+
+
+  export {Project, OpenProject}
+
