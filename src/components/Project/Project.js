51a577fbd5fa66ee560f21bdd0db1d09751046c0
GRitchie1 <gregor.homeautomation@gmail.com>
2022-06-21 10:06:52 +0100

    Show contact me button if project URL is empty

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -43,7 +43,8 @@ class Project extends React.Component {
           <img className={styles.OpenProjectImg} src={this.img} alt={this.title}></img>
           <h2 className={styles.ProjectText}>{this.title}</h2>
           <p className={styles.ProjectText}>{this.content}</p>
-          <Button link={this.url} text="Check it out on GitHub"/>
+          {this.url ? <Button link={this.url} text="Check it out on GitHub"/> : <Button link='#contactme' text="Contact Me"/>}
+          
         </div>
       );
     }
